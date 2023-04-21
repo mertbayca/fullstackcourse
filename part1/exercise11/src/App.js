8aed9a14a435b8c98c067c02c91445469c2890d1
mertbayca <103841104+mertbayca@users.noreply.github.com>
2023-04-21 13:09:04 +0300

    necessary commit

---
+++ part1/exercise11/src/App.js
@@ -0,0 +1,13 @@
+const App = () => {
+  // const-definitions
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content ... />
+      <Total ... />
+    </div>
+  )
+}
+
+export default App
---
+++ part1/exercise11/src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Content = () {
+    return (
+      <div>
+        <Part />
+        <Part />
+        <Part />
+      </div>
+    )
+}
+  
+
+export default Content
